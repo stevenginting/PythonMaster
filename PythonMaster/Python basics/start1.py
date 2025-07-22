138822e7eba8bd8b5887db22913c1fea6eb714bc
StevenSeagle <100983480+stevenginting@users.noreply.github.com>
2025-07-22 20:03:02 +0700

    Add files via upload

---
+++ PythonMaster/Python basics/start1.py
@@ -0,0 +1,10 @@
+#Basic 
+#syntax
+print("Hello world")
+print("Hello my friend")
+print("I want to be an engineer")
+
+a = 123 
+b = 'string'
+
+print(type(a))
---
+++ PythonMaster/Python basics/start2.py
@@ -0,0 +1,27 @@
+#Variable
+#variable has a number value
+a = 10
+b = 5 
+#variable has a string value
+x = "seagle"
+y = "hansome"
+print(x,y)
+
+#VARIABLE NAMES
+name = "jhon"
+#name 2 = 'jeni' #<---- its wrong you cant use it
+#name-2 = "jeni" #<---- its wrong you cant use it
+
+#If you want to use two or more words for a variable, use "_"
+name_3 = "george" #its correct
+name_3_5_6 = "seagle,jhon,jeni" #its correct
+
+#Variable multiple value
+
+#we start with names of fruits
+x,y,z = ["apple","banana","cherry"] #list , and python most start 0 
+fruits = x,y,z
+print(x)
+print(y)
+print(z)
+print(fruits)
